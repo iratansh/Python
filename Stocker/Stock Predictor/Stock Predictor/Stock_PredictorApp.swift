9d09bc647a4a54aa6ef516d85d68c2092e06a688
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-05-13 19:51:51 -0600

    Add files via upload

---
+++ Stocker/Stock Predictor/Stock Predictor/Stock_PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_PredictorApp.swift
+//  Stock Predictor
+//
+//  Created by Ishaan Ratanshi on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct Stock_PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
