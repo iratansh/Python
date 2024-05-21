884821aa9bd63f65ba2f7b753b234a3dd82cc716
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-05-21 11:44:49 -0600

    Update BNN.py

--- Stocker/BNN.py
+++ Stocker/BNN.py
@@ -1,1 +1,67 @@
+import pyro
+import pyro.distributions as dist
+from pyro.nn import PyroModule, PyroSample
+import torch.nn as nn
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from copy import deepcopy as dc
 
+class BNN(PyroModule):
+    def __init__(self, in_dim = 1, out_dim = 1, hid_dim = 5, prior_scale = 10):
+        super().__init__()
+
+        self.activation = nn.Tanh()
+        self.layer1 = PyroModule[nn.Linear](in_dim, hid_dim) 
+        self.layer2 = PyroModule[nn.Linear](hid_dim, out_dim)
+
+        self.layer1.weight = PyroSample(dist.Normal(0., prior_scale).expand([hid_dim, in_dim]).to_event(2))
+        self.layer1.bias = PyroSample(dist.Normal(0., prior_scale).expand([hid_dim]).to_event(1))
+        self.layer2.weight = PyroSample(dist.Normal(0., prior_scale).expand([out_dim, hid_dim]).to_event(2))
+        self.layer2.bias = PyroSample(dist.Normal(0., prior_scale).expand([out_dim]).to_event(1))
+
+    def forward(self, x, y=None):
+        x = x.reshape(-1, 1)
+        x = self.activation(self.layer1(x))
+        mu = self.layer2(x).squeeze()
+        sigma = pyro.sample("sigma", dist.Gamma(.5, 1)) 
+
+        with pyro.plate("data", x.shape[0]):
+            obs = pyro.sample("obs", dist.Normal(mu, sigma * sigma), obs=y)
+        return mu
+    
+def readDataFromFile(csv):
+    df = pd.read_csv(csv, usecols=['Date', 'Close'])
+    return df
+
+def process_dataframe(df, n_steps):
+    df = dc(df)
+    df.set_index('Date', inplace=True)
+    for i in range(1, n_steps + 1):
+        df[f'Close(t-{i})'] = df['Close'].shift(i)
+    df.dropna(inplace=True)
+    return df
+
+def scale_dataframe(shifted_df):
+    shifted_df_as_np = shifted_df.to_numpy()
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    shifted_df_as_np = scaler.fit_transform(shifted_df_as_np)
+    return shifted_df_as_np
+
+def split_data(shifted_df_as_np):
+    X = shifted_df_as_np[:, 1:]
+    y = shifted_df_as_np[:, 0]
+    return X, y
+
+def split_data_into_train_test(X, y, split_index):
+    X_train = X[:split_index]
+    X_test = X[split_index:]
+    y_train = y[:split_index]
+    y_test = y[split_index:]
+    return X_train, X_test, y_train, y_test
+
+def main():
+    model = BNN()
+    
+
+if __name__ == "__main__":
+    main()
