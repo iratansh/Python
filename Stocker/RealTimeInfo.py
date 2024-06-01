40ad1b49595d8194b94e56952807c3643dceeb19
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-05-31 19:51:52 -0600

    Update RealTimeInfo.py

--- Stocker/RealTimeInfo.py
+++ Stocker/RealTimeInfo.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 import requests
 
-def sentiment_analysis(stock):
+def get_current_stock_prices(stock):
     today = date.today()
     week_ago = today - timedelta(days=7)
     API = 'https://api.polygon.io/v2/aggs/ticker/'+ stock + '/range/1/day/' + str(week_ago) +'/' + str(today) + '?apiKey=kdqtByvNbe9w6AXHo3feX4F6K7eyQIJ1'
