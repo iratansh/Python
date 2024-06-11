e1863aa378661c1794fe81d15021c56498a9582d
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-06-10 20:55:21 -0600

    Add files via upload

---
+++ Stocker/Stocker IOs App/Stocker IOs App/LoadingView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct LoadingView: View {
+    @State private var dotCount: Int = 1
+    
+    var body: some View {
+        VStack {
+            Text("Stocker")
+                .font(.system(size: 32, weight: .medium, design: .default))
+                .foregroundColor(.black)
+            
+            HStack(spacing: 0) {
+                Text("Loading")
+                    .font(.system(size: 18, weight: .medium, design: .default))
+                    .foregroundColor(.black)
+                Text(String(repeating: ".", count: dotCount))
+                    .font(.system(size: 18, weight: .medium, design: .default))
+                    .foregroundColor(.black)
+                    .onAppear {
+                        startLoadingAnimation()
+                    }
+            }
+        }
+    }
+    
+    private func startLoadingAnimation() {
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
+            dotCount = (dotCount % 3) + 1
+        }
+    }
+}
