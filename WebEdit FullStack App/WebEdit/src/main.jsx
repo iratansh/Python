bbd94503cb8a28106e2d952be5f7c8f8b2557da3
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-06-13 13:43:36 -0600

    Update main.jsx

--- WebEdit FullStack App/WebEdit/src/main.jsx
+++ WebEdit FullStack App/WebEdit/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { useState } from 'react'
+import NavigationBar from './Navbar'
+import AppLaunch from './AppLaunch'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return (
+    <>
+    <div className='main'>
+       <AppLaunch/>
+      </div>
+    </>
+  )
+}
+
+export default App
