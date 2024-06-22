51cdaad587bb0e1bb8d7b23a98d624c93b299b5c
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-06-22 10:26:07 -0600

    Update Navbar.jsx

--- WebEdit FullStack App/WebEdit/src/Navbar.jsx
+++ WebEdit FullStack App/WebEdit/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -6,19 +6,11 @@ import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   FaUpload,
-  FaDownload,
   FaFileAlt,
   FaPrint,
   FaEnvelope,
-  FaShareAlt,
   FaUndo,
   FaRedo,
-  FaFont,
-  FaTextHeight,
-  FaPalette,
-  FaHighlighter,
-  FaBold,
-  FaItalic,
   FaUnderline,
   FaStrikethrough,
   FaSubscript,
@@ -35,54 +27,50 @@ import {
   FaTable,
   FaCode,
   FaComment,
-  FaTasks,
   FaArrowsAlt,
   FaFilePdf,
   FaFileCsv,
-  FaRegWindowMaximize,
-  FaRegWindowMinimize,
-  FaTrash,
-  FaHistory,
   FaTag,
-  FaUser,
-  FaShare,
   FaCog,
   FaQuestionCircle,
   FaSearchPlus,
   FaSearchMinus,
-  FaCut,
-  FaCopy,
-  FaPaste,
 } from "react-icons/fa";
-import NestedNavbar from "./NestedNavbar";
 
-export default function NavigationBar({ onHelpClick }) {
-  const [docTitle, setDocTitle] = useState("Untitled Document");
-  const fileInputRef = useRef(null);
-
-  const handleTitleChange = (e) => {
-    setDocTitle(e.target.value);
-  };
-
-  const handleUploadClick = () => {
-    fileInputRef.current.click();
-  };
-
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const fileContent = event.target.result;
-        console.log(`Uploaded file content: ${fileContent}`);
-      };
-      reader.readAsText(file);
-    }
-  };
-
+export default function NavigationBar({
+  docTitle,
+  onTitleChange,
+  onNewDocument,
+  onUploadClick,
+  onEmailClick,
+  onPrintClick,
+  onSaveAsPDF,
+  onSaveAsCSV,
+  onUndo,
+  onRedo,
+  onBoldClick,
+  onUnderlineClick,
+  onStrikethroughClick,
+  onSubscriptClick,
+  onSuperscriptClick,
+  onInsertImageClick,
+  onAlignLeftClick,
+  onAlignCenterClick,
+  onAlignRightClick,
+  onJustifyClick,
+  onIndentClick,
+  onOutdentClick,
+  onInsertLinkClick,
+  onUnlinkClick,
+  onInsertTableClick,
+  onInsertCodeBlockClick,
+  onInsertCommentClick,
+  onFullScreenClick,
+  onSettingsClick,
+  onHelpClick,
+}) {
   return (
     <>
-      <NestedNavbar />
       <Navbar
         fixed="top"
         style={{
@@ -97,217 +85,134 @@ export default function NavigationBar({ onHelpClick }) {
           <Form.Control
             type="text"
             value={docTitle}
-            onChange={handleTitleChange}
+            onChange={onTitleChange}
             style={{ width: "300px", marginRight: "20px" }}
           />
         </Form>
-        <Nav style={{marginRight:"700px"}}>
+        <Nav style={{ marginRight: "700px" }}>
           <NavDropdown title="File" id="file-dropdown">
-            <NavDropdown.Item href="#new">
+            <NavDropdown.Item onClick={onNewDocument}>
               <FaFileAlt style={{ marginRight: "10px" }} />
               New
             </NavDropdown.Item>
-            <NavDropdown.Item onClick={handleUploadClick}>
+            <NavDropdown.Item onClick={onUploadClick}>
               <FaUpload style={{ marginRight: "10px" }} />
               Upload
             </NavDropdown.Item>
-            <NavDropdown.Item href="#rename">
-              <FaFileAlt style={{ marginRight: "10px" }} />
-              Rename
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#download">
-              <FaDownload style={{ marginRight: "10px" }} />
-              Download
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#email">
+            <NavDropdown.Item onClick={onEmailClick}>
               <FaEnvelope style={{ marginRight: "10px" }} />
               Email
             </NavDropdown.Item>
-            <NavDropdown.Item href="#print">
+            <NavDropdown.Item onClick={onPrintClick}>
               <FaPrint style={{ marginRight: "10px" }} />
               Print
             </NavDropdown.Item>
-            <NavDropdown.Item href="#share">
-              <FaShareAlt style={{ marginRight: "10px" }} />
-              Share
-            </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#save-as-pdf">
+            <NavDropdown.Item onClick={onSaveAsPDF}>
               <FaFilePdf style={{ marginRight: "10px" }} />
               Save as PDF
             </NavDropdown.Item>
-            <NavDropdown.Item href="#save-as-csv">
+            <NavDropdown.Item onClick={onSaveAsCSV}>
               <FaFileCsv style={{ marginRight: "10px" }} />
               Save as CSV
             </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#maximize">
-              <FaRegWindowMaximize style={{ marginRight: "10px" }} />
-              Maximize
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#minimize">
-              <FaRegWindowMinimize style={{ marginRight: "10px" }} />
-              Minimize
-            </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Edit" id="edit-dropdown">
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onUndo}>
               <FaUndo style={{ marginRight: "10px" }} />
               Undo
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onRedo}>
               <FaRedo style={{ marginRight: "10px" }} />
               Redo
             </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item>
-              <FaCut style={{ marginRight: "10px" }} />
-              Cut
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaCopy style={{ marginRight: "10px" }} />
-              Copy
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaPaste style={{ marginRight: "10px" }} />
-              Paste
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>
-              <FaBold style={{ marginRight: "10px" }} />
+            <NavDropdown.Item onClick={onBoldClick}>
+              <FaUnderline style={{ marginRight: "10px" }} />
               Bold
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaItalic style={{ marginRight: "10px" }} />
-              Italic
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaUnderline style={{ marginRight: "10px" }} />
+            <NavDropdown.Item onClick={onUnderlineClick}>
+              <FaStrikethrough style={{ marginRight: "10px" }} />
               Underline
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onStrikethroughClick}>
               <FaStrikethrough style={{ marginRight: "10px" }} />
               Strikethrough
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onSubscriptClick}>
               <FaSubscript style={{ marginRight: "10px" }} />
               Subscript
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onSuperscriptClick}>
               <FaSuperscript style={{ marginRight: "10px" }} />
               Superscript
             </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="View" id="view-dropdown">
-            <NavDropdown.Item>
-              <FaSearchPlus style={{ marginRight: "10px" }} />
-              Zoom In
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaSearchMinus style={{ marginRight: "10px" }} />
-              Zoom Out
-            </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onFullScreenClick}>
               <FaArrowsAlt style={{ marginRight: "10px" }} />
               Full Screen
             </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Insert" id="insert-dropdown">
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onInsertImageClick}>
               <FaFileImage style={{ marginRight: "10px" }} />
               Image
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onInsertLinkClick}>
               <FaLink style={{ marginRight: "10px" }} />
               Link
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onUnlinkClick}>
               <FaUnlink style={{ marginRight: "10px" }} />
               Unlink
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onInsertTableClick}>
               <FaTable style={{ marginRight: "10px" }} />
               Table
             </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onInsertCodeBlockClick}>
               <FaCode style={{ marginRight: "10px" }} />
               Code Block
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onInsertCommentClick}>
               <FaComment style={{ marginRight: "10px" }} />
               Comment
             </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaTasks style={{ marginRight: "10px" }} />
-              Checklist
-            </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Format" id="format-dropdown">
-            <NavDropdown.Item>
-              <FaFont style={{ marginRight: "10px" }} />
-              Font
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaTextHeight style={{ marginRight: "10px" }} />
-              Font Size
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaPalette style={{ marginRight: "10px" }} />
-              Font Color
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaHighlighter style={{ marginRight: "10px" }} />
-              Highlight Color
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onAlignLeftClick}>
               <FaAlignLeft style={{ marginRight: "10px" }} />
               Align Left
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onAlignCenterClick}>
               <FaAlignCenter style={{ marginRight: "10px" }} />
               Align Center
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onAlignRightClick}>
               <FaAlignRight style={{ marginRight: "10px" }} />
               Align Right
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onJustifyClick}>
               <FaAlignJustify style={{ marginRight: "10px" }} />
               Justify
             </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onIndentClick}>
               <FaIndent style={{ marginRight: "10px" }} />
               Indent
             </NavDropdown.Item>
-            <NavDropdown.Item>
+            <NavDropdown.Item onClick={onOutdentClick}>
               <FaOutdent style={{ marginRight: "10px" }} />
               Outdent
             </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Tools" id="tools-dropdown">
-            <NavDropdown.Item>
-              <FaHistory style={{ marginRight: "10px" }} />
-              History
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaTag style={{ marginRight: "10px" }} />
-              Tags
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaUser style={{ marginRight: "10px" }} />
-              Collaborators
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaShare style={{ marginRight: "10px" }} />
-              Share
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>
-              <FaCog style={{ marginRight: "10px" }} />
+            <NavDropdown.Item onClick={onSettingsClick}>
+              <FaCog style={{ marginRight: "10px" }}/>
               Settings
             </NavDropdown.Item>
             <NavDropdown.Item onClick={onHelpClick}>
@@ -315,31 +220,10 @@ export default function NavigationBar({ onHelpClick }) {
               Help
             </NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown title="Window" id="window-dropdown">
-            <NavDropdown.Item>
-              <FaRegWindowMaximize style={{ marginRight: "10px" }} />
-              Maximize
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <FaRegWindowMinimize style={{ marginRight: "10px" }} />
-              Minimize
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>
-              <FaTrash style={{ marginRight: "10px" }} />
-              Close
-            </NavDropdown.Item>
-          </NavDropdown>
         </Nav>
       </Navbar>
-      <input
-        type="file"
-        ref={fileInputRef}
-        style={{ display: "none" }}
-        accept=".pdf,.txt"
-        onChange={handleFileUpload}
-      />
     </>
   );
 }
+
 
